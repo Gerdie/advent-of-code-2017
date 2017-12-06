ca9766e1536c160d3a33ffef5c05b783e81827a3
Maria Moy <maria.moy@radius.com>
2017-12-06 15:21:40 -0800

    Day 3 complete

--- day3/solution.py
+++ day3/solution.py
@@ -29,74 +29,95 @@ print('Part One: {}'.format(find_route_to_center(starting_val)))
 
 # PART TWO
 
-# grid = [[x, x, x],
-#         [x, x, x],
-#         [x, x, x]]
+# grid = [[n, n, n],
+#         [n, n, n],
+#         [n, n, n]]
 
 grid = [[1]]
 
-def expand_grid():
-    len_y = len(grid)
-    # start at bottom going right
-    new_cell = calculate_cell_value(len_y - 1, len_y - 1)
-    grid[-1].append(new_cell)
-    for x in range(len_y):
-        new_cell = calculate_cell_value(len_y - 1, len_y - 1)
-        grid[-1].append(new_cell)
-
-
-
-def calculate_cell_value(x, y):
+def calculate_cell_value(grid, x, y):
     total = 0
     try:
-        total += grid[x][y + 1]
+        total += grid[y + 1][x]
     except IndexError:
         pass
     try:
-        total += grid[x][y - 1]
+        total += grid[y][x + 1]
     except IndexError:
         pass
     try:
-        total += grid[x + 1][y - 1]
+        total += grid[y + 1][x + 1]
     except IndexError:
         pass
-    try:
-        total += grid[x + 1][y]
-    except IndexError:
-        pass
-    try:
-        total += grid[x + 1][y + 1]
-    except IndexError:
-        pass
-    try:
-        total += grid[x - 1][y - 1]
-    except IndexError:
-        pass
-    try:
-        total += grid[x - 1][y]
-    except IndexError:
-        pass
-    try:
-        total += grid[x - 1][y + 1]
-    except IndexError:
-        pass
-    return total
+    if y > 0:
+        try:
+            total += grid[y - 1][x]
+        except IndexError:
+            pass
+        try:
+            total += grid[y - 1][x + 1]
+        except IndexError:
+            pass
+    if x > 0 and y > 0:
+        try:
+            total += grid[y - 1][x - 1]
+        except IndexError:
+            pass
+    if x > 0:
+        try:
+            total += grid[y][x - 1]
+        except IndexError:
+            pass
+        try:
+            total += grid[y + 1][x - 1]
+        except IndexError:
+            pass
+    return grid, total
+
+def get_new_row(row_size):
+    return [0] * row_size
+
+def expand_grid(grid, highest):
+    len_y = len(grid)
+    # start at bottom and go up
+    for val in range(len_y):
+        grid, new_cell = calculate_cell_value(grid, len_y, len_y - 1 - val)
+        if new_cell > highest:
+            return new_cell
+        grid[-1 - val].append(new_cell)
+    # add to top
+    top_row = get_new_row(len_y + 1)
+    grid = [top_row] + grid
+    # calculate top from right to left
+    for val in range(len_y + 1):
+        grid, new_cell = calculate_cell_value(grid, len_y - val, 0)
+        if new_cell > highest:
+            return grid, new_cell
+        grid[0][len_y - val] = new_cell
+    # go down left side and pre-pend zero vals
+    for val in range(len_y + 1):
+        grid[val] = [0] + grid[val]
+    # calculate left side vals
+    for val in range(len_y + 1):
+        grid, new_cell = calculate_cell_value(grid, 0, val)
+        if new_cell > highest:
+            return new_cell
+        grid[val][0] = new_cell
+    # add to bottom
+    bottom_row = get_new_row(len_y + 2)
+    grid.append(bottom_row)
+    # calculate bottom vals
+    for val in range(len_y + 2):
+        grid, new_cell = calculate_cell_value(grid, val, len_y + 1)
+        if new_cell > highest:
+            return new_cell
+        grid[-1][val] = new_cell
+    return grid, None
 
 
-def expand_grid(grid):
-    current_size = len(grid[0])
-    for row in grid:
-        row = [0] + row + [0]
-    grid = [[0] * current_size] + grid
-    grid.append([0] * current_size)
+while True:
+    grid, highest = expand_grid(grid, starting_val)
+    if highest is not None:
+        break
 
-grid = [[1]]
-
-
-def part_two():
-    highest = 0
-    while True:
-        expand_grid(grid)
-        if highest > starting_val:
-            return highest
-
+print('Part Two: {}'.format(highest))
