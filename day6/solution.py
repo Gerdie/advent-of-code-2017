378733fb3d52db32dbce7416e332cf0c72448827
Maria Moy <maria.moy@radius.com>
2017-12-06 16:59:17 -0800

    Day 6 complete

--- day6/solution.py
+++ day6/solution.py
@@ -5,7 +5,6 @@ total_loops = 0
 
 with open('input.txt') as input_file:
 	memory_banks = map(int, input_file.read().split())
-	# memory_banks = [0, 2,7,0]
 	num_banks = len(memory_banks)
 	past_configurations.add(','.join([str(x) for x in memory_banks]))
 
@@ -26,3 +25,31 @@ with open('input.txt') as input_file:
 			past_configurations.add(bank_string)
 
 print('Part One: {}'.format(total_loops))
+
+# PART TWO
+
+past_configurations = {}
+total_loops = 0
+
+with open('input.txt') as input_file:
+	memory_banks = map(int, input_file.read().split())
+	num_banks = len(memory_banks)
+	past_configurations[','.join([str(x) for x in memory_banks])] = 0
+
+	while True:
+		max_amt = max(memory_banks)
+		max_bank_index = memory_banks.index(max_amt)
+		memory_banks[max_bank_index] = 0
+
+		for i in range(max_amt):
+			next_index = (max_bank_index + 1 + i) % num_banks
+			memory_banks[next_index] = memory_banks[next_index] + 1
+
+		total_loops += 1
+		bank_string = ','.join([str(x) for x in memory_banks])
+		if bank_string in past_configurations:
+			break
+		else:
+			past_configurations[bank_string] = total_loops
+
+print('Part One: {}'.format(total_loops - past_configurations[bank_string]))
