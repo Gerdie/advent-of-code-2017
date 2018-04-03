301dfae08fb41e7eb5d5754290aeaa309945463a
Gerdie <mariakmoy@gmail.com>
2018-04-02 21:54:36 -0700

    add hex map navigation

--- day11/solution.py
+++ day11/solution.py
@@ -1,51 +1,33 @@
+x = 0
+y = 0
+z = 0
 
-opposites_map = {
-    'n': 's',
-    'ne': 'sw',
-    'nw': 'se'
-}
-
-steps_map = {}
+distances = set([])
 
 with open('input.txt') as in_file:
     directions = in_file.read().split(',')
-    for direction in directions:
-        steps_map[direction] = steps_map.get(direction, 0) + 1
+    for d in directions:
+        if d == 'n':
+            y += 1
+            z -= 1
+        elif d == 's':
+            y -= 1
+            z += 1
+        elif d == 'ne':
+            x += 1
+            z -= 1
+        elif d == 'sw':
+            x -= 1
+            z += 1
+        elif d == 'nw':
+            y += 1
+            x -= 1
+        elif d == 'se':
+            x += 1
+            y -= 1
+        distance = (abs(x) + abs(y) + abs(z)) / 2
+        distances.add(distance)
 
-# eliminate opposites
-for opposite in opposites_map:
-    north_dir = steps_map.get(opposite, 0)
-    south_dir = steps_map.get(opposites_map[opposite], 0)
-    if north_dir == south_dir:
-        steps_map[opposite] = 0
-        steps_map[opposites_map[opposite]] = 0
-    elif north_dir > south_dir:
-        steps_map[opposite] = north_dir - south_dir
-        steps_map[opposites_map[opposite]] = 0
-    elif south_dir > north_dir:
-        steps_map[opposites_map[opposite]] = south_dir - north_dir
-        steps_map[opposite] = 0
 
-print(steps_map)
-
-# 2 steps 'ne' + 'nw' can be condensed as 1 step 'n'
-if steps_map.get('ne', 0) > 0 and steps_map.get('nw', 0) > 0:
-    if steps_map['ne'] > steps_map['nw']:
-        steps_map['n'] = steps_map['n'] + steps_map['nw']
-        steps_map['ne'] = steps_map['ne'] - steps_map['nw']
-        steps_map['nw'] = 0
-    elif steps_map['nw'] > steps_map['ne']:
-        steps_map['n'] = steps_map['n'] + steps_map['ne']
-        steps_map['nw'] = steps_map['nw'] - steps_map['ne']
-        steps_map['ne'] = 0
-if steps_map.get('se', 0) > 0 and steps_map.get('sw', 0) > 0:
-    if steps_map['se'] > steps_map['sw']:
-        steps_map['s'] = steps_map['s'] + steps_map['sw']
-        steps_map['se'] = steps_map['se'] - steps_map['sw']
-        steps_map['sw'] = 0
-    elif steps_map['sw'] > steps_map['se']:
-        steps_map['s'] = steps_map['s'] + steps_map['se']
-        steps_map['sw'] = steps_map['sw'] - steps_map['se']
-        steps_map['se'] = 0
-
-print('Part One: {}'.format(sum(steps_map.values())))
+print('Part One: {}'.format(distance))
+print('Part Two: {}'.format(max(distances)))
